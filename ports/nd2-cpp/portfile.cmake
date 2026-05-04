0ced7c914858dac7f87b15a81b302c15b9098db5
keejkrej <ctyjackcao@outlook.com>
2026-05-04 23:00:02 +0200

    Add vcpkg registry packaging

---
+++ ports/nd2-cpp/portfile.cmake
@@ -0,0 +1,27 @@
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL "https://github.com/keejkrej/nd2-cpp.git"
+    REF main
+)
+
+set(ND2CPP_BUILD_EXAMPLE OFF)
+if("tools" IN_LIST FEATURES)
+    set(ND2CPP_BUILD_EXAMPLE ON)
+endif()
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DND2CPP_BUILD_EXAMPLE=${ND2CPP_BUILD_EXAMPLE}
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME nd2-cpp CONFIG_PATH share/nd2-cpp)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+if("tools" IN_LIST FEATURES)
+    vcpkg_copy_tools(TOOL_NAMES nd2info AUTO_CLEAN)
+endif()
+
+vcpkg_install_copyright(COMMENT "No license file is included in the upstream nd2-cpp repository.")
